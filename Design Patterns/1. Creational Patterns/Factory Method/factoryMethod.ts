30c80cbb3b4cb5762ce5384aa1812c0befc97b73
Vincentius Devin <devin.vincentius@gmail.com>
2024-05-24 13:55:48 +0700

    factory method

---
+++ Design Patterns/1. Creational Patterns/Factory Method/factoryMethod.ts
@@ -0,0 +1,54 @@
+// Car is the interface that defines the behavior of objects created by the factory method
+interface Car {
+  use(): string;
+}
+
+// ConcreteCarA is a concrete implementation of the Car interface
+class ConcreteCarA implements Car {
+  use(): string {
+      return "Using Car A";
+  }
+}
+
+// ConcreteCarB is a concrete implementation of the Car interface
+class ConcreteCarB implements Car {
+  use(): string {
+      return "Using Car B";
+  }
+}
+
+// Creator is the interface that defines the factory method
+interface Creator {
+  factoryMethod(): Car;
+}
+
+// ConcreteCreatorA is a concrete implementation of the Creator interface that creates ConcreteCarA
+class ConcreteCreatorA implements Creator {
+  factoryMethod(): Car {
+      return new ConcreteCarA();
+  }
+}
+
+// ConcreteCreatorB is a concrete implementation of the Creator interface that creates ConcreteCarB
+class ConcreteCreatorB implements Creator {
+  factoryMethod(): Car {
+      return new ConcreteCarB();
+  }
+}
+
+function main() {
+  let creator: Creator;
+
+  // Use ConcreteCreatorA to create a Car
+  creator = new ConcreteCreatorA();
+  let carA: Car = creator.factoryMethod();
+  console.log(carA.use()); // Output: Using Car A
+
+  // Use ConcreteCreatorB to create a Car
+  creator = new ConcreteCreatorB();
+  let carB: Car = creator.factoryMethod();
+  console.log(carB.use()); // Output: Using Car B
+}
+
+// Run the main function
+main();
